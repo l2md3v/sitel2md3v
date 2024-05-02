04c9566abebf2df995a9d6663af357b6a92e02fb
l2md3v <lamdev1212@gmail.com>
2024-05-02 11:48:09 +0700

    .

--- lib/site.ts
+++ lib/site.ts
@@ -10,10 +10,10 @@ import {
 import type { Metadata, Viewport } from 'next'
 
 export const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'https://tiesen.id.vn' : 'http://localhost:3000'
+  process.env.NODE_ENV === 'production' ? 'https://l2md3vsite.vercel.app/' : 'http://localhost:3000'
 
 export const description =
-  "I'm a weeb developer who loves to build things. I enjoy working with TypeScript, Next.js, and ElysiaJS. I'm also a fan of anime, manga, and light novels."
+  "I'm a web developer who loves to build things. I enjoy working with TypeScript, Next.js, and ElysiaJS. I'm also a fan of anime, manga, and light novels."
 
 interface SiteConfig {
   meta: Metadata
